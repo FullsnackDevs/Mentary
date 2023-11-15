f550c5f77d97f9470863c0184f9bdf51c689f9d7
Le Phuc Hieu <leodavs2104@gmail.com>
2023-11-15 22:39:45 +0700

    update diary

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import Editor from "./components/Editor";
 import Bot from "./components/Bot";
 import Sidebar from "./components/Sidebar";
-import { useEffect, useState} from "react";
-
-
+import "./index.css";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(false);
 
   useEffect(() => {
-      const menuIcon = document.querySelector("i.menu-icon");
+    const menuIcons = document.querySelectorAll("button.menu-btn");
+    menuIcons.forEach((menuIcon) =>
       menuIcon.addEventListener("click", () => {
-          setState(!state);
-      });
+        setState(!state);
+      })
+    );
   });
 
-
-  return(
+  return (
     <div className="flex justify-between h-screen w-full">
-      <i className="menu-icon fa-solid fa-bars text-sm fixed top-0 left-24 z-10"></i>
-      <div className={state ? "sidebar fixed left-0 w-1/6 top-0 h-full bg-red-500" : "sidebar fixed left-0 w-16 top-0 h-full bg-red-500 invisible"}>
-          <Sidebar/>
+      <button className="menu-btn menu-icon fixed top-0 left-24 z-10">
+        <i className="fa-solid fa-bars"></i>
+      </button>
+      <div
+        className={
+          state
+            ? "sidebar fixed left-0 w-1/6 top-0 h-full bg-red-500"
+            : "sidebar fixed left-0 w-16 top-0 h-full bg-red-500 invisible"
+        }
+      >
+      <Sidebar/>
       </div>
-      <Editor/>
-      <div className="fixed right-0 top-0 w-1/6 h-full bg-gray-500">
-          <Bot/>
+      <Editor />
+      <div className="right-0 top-0 w-1/6 h-full bg-gray-500">
+        <Bot />
       </div>
     </div>
-  )
+  );
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,58 @@
-export default function Editor(){
-    return(
-        <div className="bg-yellow-200 w-full h-full">
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.bubble.css';
+
+function Editor(){
+    Editor.formats = [
+        'header',
+        'size',
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        'blockquote',
+        'list',
+        'bullet',
+        'indent',
+        'link',
+        'image',
+        'color',
+      ];
+
+      Editor.modules = {
+        toolbar: [
+          [{ 'header': '1'}, {'header': '2'}],
+          [{size: []}],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, 
+           {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image', 'video'],
+          ['clean']
+        ],
+        clipboard: {
+          // toggle to add extra line breaks when pasting HTML:
+          matchVisual: false,
+        }
+      }
+      
+
+
+    const [value, setValue] = useState('');
+
+
+
+    return (
+        <div id="editor" className='h-full w-5/6 p-12'>
+            <ReactQuill 
+                theme="bubble" 
+                value={value} onChange={setValue} 
+                className='h-screen w-full'
+                placeholder="Typing..."
+                bounds={'#editor'}
+                formats={Editor.formats}
+                modules={Editor.modules}
+            />;
         </div>
-    )
+    ) 
 }
+export default Editor
