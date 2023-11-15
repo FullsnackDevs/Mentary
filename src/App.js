16694b11c2a6569a576069f4da424c32a4563e8f
Le Phuc Hieu <leodavs2104@gmail.com>
2023-11-15 17:23:38 +0700

    new update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Editor from "./components/Editor";
+import Bot from "./components/Bot";
+import Sidebar from "./components/Sidebar";
+import { useEffect, useState} from "react";
+
+
 
 export default function App() {
+  const [state, setState] = useState(true);
+
+  useEffect(() => {
+      const menuIcon = document.querySelector("i.menu-icon");
+      menuIcon.addEventListener("click", () => {
+          setState(!state);
+      });
+  });
+
+
   return(
     <div className="flex justify-between h-screen w-full">
-      <Editor>
-      </Editor>
+      
+      <div className={state ? "sidebar fixed left-0 w-1/6 top-0 h-full bg-red-500" : "sidebar fixed left-0 w-16 top-0 h-full bg-red-500 invisible"}>
+          <Sidebar/>
+      </div>
+      <Editor/>
+      <div className="fixed right-0 top-0 w-1/6 h-full bg-gray-500">
+          <Bot/>
+      </div>
     </div>
   )
 }
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,25 +1,6 @@
-import Sidebar from "./Sidebar";
-import Bot from "./Bot";
-import { useEffect, useState} from "react";
-
 export default function Editor(){
-    const [state, setState] = useState(true);
-
-    useEffect(() => {
-        const menuIcon = document.querySelector("i.menu-icon");
-        menuIcon.addEventListener("click", () => {
-            setState(!state);
-        });
-    });
     return(
         <div className="bg-yellow-200 w-full h-full">
-            <div className={state ? "sidebar fixed left-0 w-1/6 top-0 h-full bg-red-500" : "sidebar fixed left-0 w-16 top-0 h-full bg-red-500 invisible"}>
-                <Sidebar></Sidebar>
-            </div>
-            
-            <div className="fixed right-0 top-0 w-1/6 h-full bg-gray-500">
-                <Bot></Bot>
-            </div>
             <i className="menu-icon fa-solid fa-bars text-sm fixed top-0 left-24"></i>
         </div>
     )
